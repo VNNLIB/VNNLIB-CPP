2436845587a6fbfb0d378c97a094a13c8ce9c4cb
Looi Yong Lun <looiyonglun@gmail.com>
2026-08-17 15:57:15 +0800

    feat: add verification result type

--- include/Solver.h
+++ include/Solver.h
@@ -7,10 +7,18 @@
 
 namespace vnnlib::solver {
 
+enum class VerificationResult {
+    Sat,
+    Unsat,
+    Unknown,
+    TimedOut
+};
+
+
+
 class VNNLIB_API Solver {
     private:
     std::string executable_;
-
 
     public:
     explicit Solver(const std::string& executable);
